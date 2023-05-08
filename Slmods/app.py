87a31dff28fd7deb508c69f5e11364d5024826fb
Sanali209 <38997864+Sanali209@users.noreply.github.com>
2023-05-08 09:30:13 +0300

    start

---
+++ Slmods/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Мое Streamlit приложение")
+
+    # Добавьте здесь свой код для взаимодействия с пользователем и обработки данных
+
+    st.write("Это простое Streamlit приложение!")
+
+if __name__ == "__main__":
+    main()
