513fe4c3b968139fda7dbc7e327bd72531d6d8bd
Sanali209 <38997864+Sanali209@users.noreply.github.com>
2023-05-10 05:22:39 +0300

    c

--- anotationmanager.py
+++ anotationmanager.py
@@ -1,36 +1,117 @@
-from pydrive.auth import GoogleAuth
+import os
+
+from transformers import pipeline
+
+
+# function for get all images from folder recursively
+def getImagesFromFolder(folder):
+    images = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(".jpg") or file.endswith(".png"):
+                images.append(os.path.join(root, file))
+    return images
+
 
 class AnotationManager:
     def __init__(self) -> None:
-        self.anotationJobs=[]
+        self.anotationJobs = []
 
-
-    def addAnotationJob(self,anotationJob):
+    def addAnotationJob(self, anotationJob):
         self.anotationJobs.append(anotationJob)
 
-    def removeAnotationJob(self,anotationJob):
+    def removeAnotationJob(self, anotationJob):
         self.anotationJobs.remove(anotationJob)
 
+
+class AnotationClass:
+    def __init__(self) -> None:
+        self.label = ""
 
 
 class AnotationJob:
     def __init__(self) -> None:
-        self.name=""
-        self.importPath=""
-        self.exportPath=""
-        self.anotations=[]
-        self.anotationItems=[]
+        self.name = ""
+        self.jobfolder = "/rawdb/AJobs/quality"
+        self.anotationclassSaveFile = "anotationclass.json"
+        self.anotationItemsSaveFile = "anotationitems.json"
+        self.transformerPipelineName = "default"
+        self.transformerPipeline = pipeline(self.transformerPipelineName)
+
+        self.importPath = ""
+        self.exportPath = ""
+        self.anotations = []
+        self.anotationItems = []
 
     def ImportAnotationsFromGoolgeDrive(self):
-        pass
+        images = getImagesFromFolder(self.importPath)
+        for image in images:
+            anotationItem = AnotationItem()
+            anotationItem.path = image
+            self.anotationItems.append(anotationItem)
 
+    def addAnotation(self, aname):
+        nanotation = AnotationClass()
+        nanotation.label = aname
+        self.anotations.append(nanotation)
 
+    def saveAnotationsClasses(self):
+        JsonList = {}
+        for cannot in self.anotations:
+            JsonList[cannot.label] = cannot.label
 
-class Anotation:
-    def __init__(self) -> None:
-        self.label=""
+        import json
+        with open(self.jobfolder + "/" + self.anotationclassSaveFile, 'w') as outfile:
+            json.dump(JsonList, outfile)
+
+    def loadAnotationsClasses(self):
+        import json
+        if not os.path.exists(self.jobfolder + "/" + self.anotationclassSaveFile):
+            return
+        with open(self.jobfolder + "/" + self.anotationclassSaveFile) as json_file:
+            data = json.load(json_file)
+            for cannot in data:
+                nanotation = AnotationClass()
+                nanotation.label = cannot
+                self.anotations.append(nanotation)
+
+    def saveAnotationsItems(self):
+        JsonList = {}
+        for cannot in self.anotationItems:
+            JsonList[cannot.path] = cannot.path
+
+        import json
+        with open(self.jobfolder + "/" + self.anotationItemsSaveFile, 'w') as outfile:
+            json.dump(JsonList, outfile)
+
+    def loadAnotationsItems(self):
+        import json
+        if not os.path.exists(self.jobfolder + "/" + self.anotationItemsSaveFile):
+            return
+        with open(self.jobfolder + "/" + self.anotationItemsSaveFile) as json_file:
+            data = json.load(json_file)
+            for cannot in data:
+                nanotation = AnotationItem()
+                nanotation.path = cannot
+                self.anotationItems.append(nanotation)
+    def NextAnotationItem(self):
+        for item in self.anotationItems:
+            if not item.anotated and not item.passed:
+                return item
+        return None
+
 
 class AnotationItem:
     def __init__(self) -> None:
-        self.path=""
-        self.anotations=[]
+        self.path = ""
+        self.anotations = []
+        self.anotated = False
+        self.passed = False
+
+curentJob = AnotationJob()
+curentJob.loadAnotationsClasses()
+curentJob.loadAnotationsItems()
+
+curentItem = curentJob.NextAnotationItem()
+
+print(len(curentJob.anotations))
